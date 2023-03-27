ff74812af9943ec354f491dd471ebfd483f6d512
dscrogg399 <dscrogg1@uab.edu>
2023-03-27 16:20:27 -0500

    removed duplicate appliance type model

--- api/models.py
+++ api/models.py
@@ -1,20 +1,6 @@
 from django.db import models
 
 # Create your models here.
-class Appliance_Type(models.Model):
-    id = models.BigAutoField(primary_key=True)
-    title = models.CharField(max_length=50)
-    wattage = models.IntegerField()
-    gallons = models.IntegerField()
-    is_active = models.BooleanField()
-
-    @classmethod
-    def create(cls, title, wattage, gallons, is_active):
-        appliance = cls(title = title, wattage = wattage, gallons = gallons, is_active = is_active)
-        return appliance
-    
-    def __str__(self):
-        return self.id
 
 #These are test models for the API and just serve as examples
 # class User(models.Model):
