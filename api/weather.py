ac5e3338977a13cd8606d393d9390a2fc17b94f9
choboribi <jacksonahyde@gmail.com>
2023-04-02 18:59:10 -0500

    added datetime to data

--- api/weather.py
+++ api/weather.py
@@ -1,27 +1,37 @@
 # functions that will be used to populate tables with history of temperature data
 from datetime import datetime, timedelta
 from meteostat import Hourly, Point
+import csv
 
 #creates a point for Birmingham, AL
 birmingham = Point(33.5186, -86.8104)
 
 # gets hourly data for the past week, excluding the current hour, 168 entries
 def getWeeklyHistory():
-    end = datetime.now() - timedelta(hours=1)
+    end = datetime.now()
     beginning = end - timedelta(days = 7)
 
     data = Hourly(birmingham, beginning, end)
     data = data.fetch()
     data_trim = data['temp'].tolist()
 
-    return(data_trim)
+    data_trim_datetime = []
 
+    for i in range(len(data_trim)):
+        data_trim_datetime.append([beginning + timedelta(hours=i), data_trim[i]])
+
+    return(data_trim_datetime)
+
+# gets data for the current hour
 def getTemperature():
     now = datetime.now()
 
     data = Hourly(birmingham, now - timedelta(hours=1), now)
     data = data.fetch()['temp'].tolist()
 
-    return(data)
+    data_datetime = [datetime.now(), data[0]]
 
+    return(data_datetime)
+
+print(getWeeklyHistory())
 print(getTemperature())
